404959355bf356c5997a0479a812966269462af4
Thiago Parizi <thiagoparizi@gmail.com>
2016-07-04 20:36:36 +0200

    First addition
    First commit

--- interactive.js
+++ interactive.js
@@ -1,74 +1,74 @@
-// - - - - - - - - - - - TAGS AND NAV STEPS
 $(document).ready(function () {
-	$(".tags").click(function() {
-	    var tagId = $(this).attr("id");
-	    $("." + tagId).toggleClass("half-opq");
-
-	    $(this).toggleClass("tags-selected");
-	    $(this).children(".fa").toggleClass("rotate45");
-	        if ($('.tags').hasClass('tags-selected')) {
-	            $('.stepSwitchDown').removeClass('tag-inctv');
-	            $('.stepTwo').addClass("stepActive");
-	        }
-	        else {
-	            $('.stepSwitchDown').addClass('tag-inctv');
-	            $('.stepTwo').removeClass("stepActive");
-	        }
-
-	    var tagToggle = $('.tags.tags-selected').length;
-	    $('.stepTwo').text(function() {
-	        if ($('.tags.tags-selected').length >= 1) {
-	            return (tagToggle);
-	        }
-	        else {
-	            return 'Choose your company\'s tags';
-	        }
-	    });
-	});
-
-	// - - - - - - - - - - - MAIN/MISSIONS AND NAV STEPS
 	$(".nav-body").hide();
-	$('.post').click(function() {
-	    $('.post').not(this).slideToggle(600);
-
-	    var firstThis = $(this);
-	    $(this).children('.mssShow').slideToggle(800, function() {
-	        $('.par-hover').text(function () {
-	            return $(this).prev().is(":visible") ? "Choose another statement" : "Choose this statement";
-	        });
-	    });
-	    
-	    $('.stepOne').toggleClass("stepActive");
-	    var idString = $(firstThis).attr("id");
-	    var stepActive = $('.stepOne').hasClass('stepActive');
-	    $('.stepOne').text(function () {
-	        if (stepActive) {
-	            $(".nav-mobile").children(".fa").toggleClass("rotate180");
-	            $(".nav-body").slideToggle(500);
-	            $('.stepSwitch').removeClass('tag-inctv');
-	            return (idString);
-	        }
-	        else {
-	            $(".nav-mobile").children(".fa").toggleClass("rotate180");
-	            $(".nav-body").slideToggle(500);
-	            $('.stepSwitch').addClass('tag-inctv');
-	            $('.stepSwitchDown').addClass('tag-inctv');
-	            $('.tags').removeClass('tags-selected');
-	            $('span').removeClass('half-opq');
-	            $('.stepTwo').replaceWith('<p class="par stepTwo">Choose your company\'s tags</p>');
-	            return 'Choose your company\'s statement';
-	        }
-	    });
+	$(".download").hide();
+	$(".mssShow").hide();
+	$(".step-inner:nth-child(2), .step-inner:nth-child(3)").addClass("tag-inctv");
+	$('.post').click(function () {
+		var post = this;
+    $(this).find('.mssShow').slideToggle(400, function () {
+      $('.post').not(post).slideToggle(200);
+      $('.button').text(function () {
+        return $(this).prev().is(":visible") ? "Choose another statement" : "Choose this statement";
+      });
+    });
+    $('.step-one').toggleClass("step-active");
+    var idString = $(this).attr("id");
+    var stepActive = $('.step-one').hasClass('step-active');
+    $('.step-one').text(function () {
+      if (stepActive) {
+        $(".nav-body").slideToggle(200);
+        $(".download").slideToggle(200);
+        $('.step-switch').removeClass('tag-inctv');
+        return (idString);
+      } else {
+        $(".nav-body").slideToggle(200);
+        $(".download").slideToggle(200);
+        $('.step-switch').addClass('tag-inctv');
+        $('.tags').removeClass('tags-selected');
+        $('span').removeClass('fadeout');
+        $('.step-two').replaceWith('<p class="par step-two">Choose some tags</p>');
+        $('.download').removeClass('step-active');
+        $('.download').hide();
+        return 'Choose a statement';
+      }
+    });
+	});
+	
+	$(".tags").click(function () {
+    var tagId = $(this).attr("id");
+    $("." + tagId).toggleClass("fadeout");
+    $(this).toggleClass("tags-selected");
+    if ($('.tags').hasClass('tags-selected')) {
+      $('.step-two').addClass("step-active");
+      $('.download').addClass("step-active");
+      $('.step-switch-down').removeClass('tag-inctv');
+    } else {
+      $('.step-two').removeClass("step-active");
+      $('.download').removeClass("step-active");
+      $('.step-switch-down').addClass('tag-inctv');
+    }
+    var tagLength = $('.tags-selected').length;
+    $('.step-two').text(function () {
+      if (tagLength >= 1) {
+        return (tagLength);
+      } else {
+        return 'Choose some tags';
+      }
+    });
 	});
 
-	// - - - - - - - - - - - EXPAND MOBILE NAVIGATION
-	$(".nav-mobile").click(function() {
-	    $(".nav-mobile").children(".fa").toggleClass("rotate180");
-	    $(".nav-body").slideToggle(500);
-	});
-
-	// - - - - - - - - - - - EXPAND MOBILE NAVIGATION
-	$(".fa-download").click(function() {
-	    alert('Your file is being donwloaded!');
+	$(".download").click(function () {
+    if ($('.tags').hasClass('tags-selected')) {
+      alert('Your file is being donwloaded!');
+    } else {
+      return false;
+    }
 	});
 });
+
+
+
+
+
+
+
